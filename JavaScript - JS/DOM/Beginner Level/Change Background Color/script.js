d44954db2e2c935b029be93a79d408ed7330133b
Ayush Kohre <kohreaayush@gmail.com>
2025-02-21 14:46:47 +0530

    Change Background Color!

---
+++ JavaScript - JS/DOM/Beginner Level/Change Background Color/script.js
@@ -0,0 +1,22 @@
+const btn = document.getElementById("btn");
+const h4Text = document.getElementById("texH");
+
+function getColor() {
+  const colorCode = "0123456789ABCDEF";
+  let hexColor = "#";
+
+  for (let i = 0; i < 6; i++) {
+    hexColor += colorCode[Math.floor(Math.random() * 16)];
+  }
+  return hexColor;
+}
+
+btn.addEventListener("mouseover", () => {
+  btn.style.cursor = "pointer";
+});
+
+btn.addEventListener("click", () => {
+  document.body.style.backgroundColor = getColor();
+  btn.textContent = "More Color";
+  h4Text.textContent = getColor();
+});
