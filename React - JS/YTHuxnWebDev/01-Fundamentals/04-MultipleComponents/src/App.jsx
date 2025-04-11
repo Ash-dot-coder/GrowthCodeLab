dd443ced1fefa85f7130cdae7d5b775dd1437996
Ayush Kohre <kohreaayush@gmail.com>
2025-04-12 02:37:10 +0530

    04-Multiple Components

---
+++ React - JS/YTHuxnWebDev/01-Fundamentals/04-MultipleComponents/src/App.jsx
@@ -0,0 +1,32 @@
+// import Add from "./components/Add"
+// import Greetings from "./components/Greetings"
+
+
+// const App = () => {
+//   return (
+//     <>
+//       <Greetings />
+//       <Add />
+//     </>
+//   )
+// }
+
+// export default App;
+
+// ----Exercise-Task----
+
+import Footer from "./exercises/Footer"
+import Header from "./exercises/Header"
+import MainContent from "./exercises/MainContent"
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  )
+}
+
+export default App;
