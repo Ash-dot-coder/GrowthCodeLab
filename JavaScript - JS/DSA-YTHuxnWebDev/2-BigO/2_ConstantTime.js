77bcc56892062dffb29f1a5cf351d75321711c0e
Ayush Kohre <kohreaayush@gmail.com>
2025-05-29 04:27:42 +0530

    DSA - BigO[2. Constant Time]

---
+++ JavaScript - JS/DSA-YTHuxnWebDev/2-BigO/2_ConstantTime.js
@@ -0,0 +1,8 @@
+// const numbers = [9, 4, 1, 6, 7, 2, 8];
+// const getElement = (arr, index) => arr[index];
+// console.log(getElement(numbers, 4))
+
+// Exercise
+const dost = ['Kartik', 'Lucky', 'Mane', 'AnkurBhaiya', 'Feeling'];
+const getDostElement = (arr, index) => arr[index];
+console.log(getDostElement(dost, 4));
