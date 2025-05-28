07eb9fa1a0b0d7f36d46be9ac311c5403e45a4ab
Ayush Kohre <kohreaayush@gmail.com>
2025-05-29 05:12:26 +0530

    DSA - Arrays[1 - Intro]

---
+++ JavaScript - JS/DSA-YTHuxnWebDev/3-Arrays/1-Intro/index.js
@@ -0,0 +1,9 @@
+const stringArr = ["a", "b", "c", "d", "e"];
+const numArr = [1, 2, 3, 4, 5];
+const boolArr = [false, true];
+const mixed = ["a", 2, false, undefined, null, { a: "a" }, ["c"]];
+
+// console.log(stringArr);
+// console.log(numArr);
+// console.log(boolArr);
+console.table(mixed);
