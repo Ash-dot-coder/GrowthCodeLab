32c798dac40ba4804196d72c55683bf545b58c2c
Ayush Kohre <kohreaayush@gmail.com>
2025-06-18 03:01:38 +0530

    NodeJS - 07[FS Module(2. Callback Api)]

---
+++ NODE-JS/YT-HUXNWEBDev/07-FSModule/2_callbackAPI/index.js
@@ -0,0 +1,73 @@
+// File Stystem - Callback API
+import { error } from "console";
+import * as fs from "fs";
+
+// 1. Creating Directory : "mkdir"
+// fs.mkdir(
+//   "C:\\Geekster - Practice\\OWN-Practice\\node-js\\yt-huxnwebdev\\07-fsmodule\\2_callbackapi\\filesystem",
+//   (error) => {
+//     if (error) throw error;
+//     console.log("Directory Created...");
+//   }
+// );
+
+// 2. Creating Directory - Path not required : "mkdir"
+// fs.mkdir(
+//   "C:Geekster - PracticeOWN-Practice\\node-jsyt-huxnwebdev\\07-fsmodule\\2_callbackapi\\testdemo",
+//   { recursive: true },
+//   (error) => {
+//     if (error) throw error;
+//     console.log("Directory Created..");
+//   }
+// );
+
+// 3. Read content of directory : "readdir"
+// fs.readdir(
+//   "C:\\Geekster - Practice\\OWN-Practice\\node-js\\yt-huxnwebdev\\07-fsmodule\\2_callbackapi",
+//   (error, files) => {
+//     if (error) throw error;
+//     for (const file of files) {
+//       console.log(file);
+//     }
+//   }
+// );
+
+// 4. Remove Directory - Directory should be empty. "rmdir"
+// fs.rmdir(
+//   "C:\\Geekster - Practice\\OWN-Practice\\node-js\\yt-huxnwebdev\\07-fsmodule\\2_callbackapi\\f",
+//   (error) => {
+//     if (error) throw error;
+//     console.log("Directory Removed");
+//   }
+// );
+
+// 5. Create & Write File : "writeFile"
+// fs.writeFile("readme.txt", "Hey NodeJs", (error) => {
+//   if (error) throw error;
+//   console.log("File Created ..");
+// });
+
+// 6. Read File : "readFile"
+// fs.readFile("readme.txt", "utf-8", (error, data) => {
+//   if (error) throw error;
+//   console.log(data);
+// });
+
+// 7. Append Data into file : "appendFile"
+// fs.appendFile("readme.txt", "Ash Coder", (error) => {
+//   if (error) throw error;
+//   console.log("Data Appended..");
+// });
+
+// 8. Copy file : "copyFile"
+// fs.copyFile("readme.txt", "info.txt", (error) => {
+//   if (error) throw error;
+//   console.log("File Copied..");
+// });
+
+// 9. Get File Information : "stat"
+fs.stat("info.txt", (error, stats) => {
+  if (error) throw error;
+  console.log("isDirectory: " + stats.isDirectory());
+  console.log("isFile: " + stats.isFile());
+});
