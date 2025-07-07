f1ff3240c396417334f015362d7ccd60c9473f62
Ayush Kohre <kohreaayush@gmail.com>
2025-07-07 14:02:00 +0530

    DSA[Phase - 1: Print(Triangle Pattern)]

---
+++ DSA[DataStructureAlgorithm]/JAVA/Phase-1/Triangle_Pattern1.java
@@ -0,0 +1,15 @@
+// Print Pattern Triangle
+// Run: java <File-Name>
+
+public class Triangle_Pattern1 {
+
+    public static void main(String[] args) {
+        int n = 6;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
