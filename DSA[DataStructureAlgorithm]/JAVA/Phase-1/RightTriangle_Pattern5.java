8d3559bbae0c6c5961010ec8efdb7f4bb10e56c4
Ayush Kohre <kohreaayush@gmail.com>
2025-07-10 11:25:50 +0530

    DSA[Phase - 1: Print(Right Triangle Pattern)]

---
+++ DSA[DataStructureAlgorithm]/JAVA/Phase-1/RightTriangle_Pattern5.java
@@ -0,0 +1,21 @@
+// Print Right Triangle Pattern
+
+public class RightTriangle_Pattern5 {
+
+    public static void main(String[] args) {
+        int n = 8;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= i; k++) {
+                if (k == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
