d34439da9fdd5a9bd6bc7728bb02c1faccce4a96
Ayush Kohre <kohreaayush@gmail.com>
2025-08-19 19:54:55 +0530

    06-Async Programming[2. Callback Hell]

---
+++ JavaScript - JS/ZeroToAdvanceYTHW_Self/06-AsyncProgramming/2-CallbackHell/liveCoding/app.js
@@ -0,0 +1,47 @@
+function callbackHell(callback) {
+  setTimeout(() => {
+    const data = "Inside (callbackHell) Function";
+    console.log(data);
+    callback(data);
+  }, 2000);
+}
+
+function firstFunc(data, callback) {
+  setTimeout(() => {
+    const processedData = `${data} - Processed First`;
+    console.log("Inside (firstFunc) Function");
+    callback(processedData);
+  }, 1000);
+}
+
+function secondFunc(data, callback) {
+  setTimeout(() => {
+    const processedData = `${data} = Processed Second`;
+    console.log("Inside (secondFunc) Function.");
+    callback(processedData);
+  }, 1500);
+}
+
+// Callback Hell
+callbackHell((data) => {
+  firstFunc(data, (processedData1) => {
+    secondFunc(processedData1, (processedData2) => {
+      console.log(`Final result of all functions: ${processedData2}`);
+    });
+  });
+});
+
+// ------------------------
+// Callback is not always asynchronous
+
+// This is the example of synchronous callback function
+console.log("Start");
+
+const numbers = [1, 2, 3, 4, 5, 6];
+
+numbers.forEach((r) => {
+  console.log(r);
+});
+
+console.log("End");
+// ------------------------
