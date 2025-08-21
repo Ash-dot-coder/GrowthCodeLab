96518377781e347e54cd66b1a59239bfd404da44
Ayush Kohre <kohreaayush@gmail.com>
2025-08-21 21:33:37 +0530

    DSA[6. Hash Table(1. Custom Hash Table)]

---
+++ JavaScript - JS/DSA-YTHuxnWebDev/6-HashTable/1-CustomHashTable/index.js
@@ -0,0 +1,72 @@
+class HashTable {
+  constructor(size = 6) {
+    this.keyMap = new Array(size);
+  }
+
+  _hashFunction(key) {
+    let sum = 0;
+    const PRIME_NUMBER = 31;
+
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      const charCode = key.charCodeAt(0) - 96;
+      sum = (sum * PRIME_NUMBER + charCode) % this.keyMap.length;
+    }
+
+    return sum;
+  }
+
+  set(key, value) {
+    const index = this._hashFunction(key);
+
+    if (!this.keyMap[index]) this.keyMap[index] = [];
+    this.keyMap[index].push([key, value]);
+    return this;
+  }
+
+  get(key) {
+    const index = this._hashFunction(key);
+    if (this.keyMap[index]) {
+      for (let i = 0; i < this.keyMap[index].length; i++) {
+        if (this.keyMap[index][i][0] === key) {
+          return this.keyMap[index][i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+
+  getAllKeys() {
+    const keys = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          keys.push(this.keyMap[i][j][0]);
+        }
+      }
+    }
+    return keys;
+  }
+
+  getAllValues() {
+    const values = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          values.push(this.keyMap[i][j][1]);
+        }
+      }
+    }
+
+    return values;
+  }
+}
+
+const phoneBook = new HashTable();
+phoneBook.set("Itachi", "888-333-893");
+phoneBook.set("Kakashi", "789-238-222");
+phoneBook.set("Obito", "289-222-892");
+phoneBook.set("MightGuy", "389-222-991");
+
+console.log(phoneBook.get("Kakashi"));
+console.log(phoneBook.getAllKeys());
+console.log(phoneBook.getAllValues());
