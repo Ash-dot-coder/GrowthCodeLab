f3f4041d6ff60d8ab667c008edd616efab1703f5
Ayush Kohre <kohreaayush@gmail.com>
2025-08-21 20:46:12 +0530

    DSA[5. Stack And Queue(3. Challenges[2_ValidParenthesis])]

---
+++ JavaScript - JS/DSA-YTHuxnWebDev/5-StackAndQueue/3-Challenges/2_ValidParenthesis.js
@@ -0,0 +1,37 @@
+const isValidParenthesis = (str) => {
+  // Use a stack to store opening brackets
+  const stack = [];
+
+  // Map opening brackets to their closing counterparts
+  const brackets = {
+    "{": "}",
+    "[": "]",
+    "(": ")",
+    "<": ">",
+  };
+
+  // Loop through each character in the string
+  for (let char of str) {
+    // If it's an opening bracket, push it to te stack
+
+    if (brackets[char]) {
+      stack.push(char);
+    } else {
+      // If it's a closing bracket, check if it matches the top of the stack
+      const top = stack.pop();
+      if (!top || brackets[top] !== char) {
+        return false;
+      }
+    }
+  }
+
+  // After iterating, check if the stack is empty (all brackets were matched)
+  return stack.length === 0;
+};
+
+console.log(isValidParenthesis("(){}[]<>"));
+console.log(isValidParenthesis("(]<}>["));
+console.log(isValidParenthesis("([)]"));
+console.log(isValidParenthesis("(<>)<{}>{[]}"));
+console.log(isValidParenthesis("<)>(]["));
+console.log(isValidParenthesis("<[{()}]>"));
