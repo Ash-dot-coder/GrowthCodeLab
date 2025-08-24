36017cda875fa3a6a18e98f71c925740d0544722
Ayush Kohre <kohreaayush@gmail.com>
2025-08-24 13:18:47 +0530

    React[Beginner(1. Components)]

---
+++ React - JS/YTJolleeJoel/Beginner/02-Practicals/01-Components/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Course from './components/course'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Course />
+    </>
+  )
+}
+
+export default App
---
+++ React - JS/YTJolleeJoel/Beginner/02-Practicals/01-Components/src/components/course.jsx
@@ -0,0 +1,15 @@
+const c1 = 'ComponentBase';
+
+function Course() {
+    return (
+        <div>
+            <img src="" alt="" />
+            <h2>This is a React's Base-1</h2>
+            <h4>Components creating Base</h4>
+            <button>Unlock this React level now</button>
+            <h2>{c1}: Using Variable{7 * 9 - 62}</h2>
+        </div>
+    );
+}
+
+export default Course;
